018c8cb383f9d3e7c544bcacafa9f6d70d8f5a04
Aqeel <aqeel03319384502@gmail.com>
2025-02-22 10:24:01 +0000

    Initial commit of Django project

--- myshop/settings.py
+++ myshop/settings.py
@@ -71,13 +71,22 @@ CKEDITOR_CONFIGS = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',  # CSRF middleware
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',  # Correct middleware
 ]
+import dj_database_url
+DATABASES = {
+    'default': dj_database_url.config(default='postgres://user:password@localhost:5432/dbname')
+}
+ALLOWED_HOSTS = ['your-railway-app.up.railway.app']
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
 
 ROOT_URLCONF = 'myshop.urls'
 
