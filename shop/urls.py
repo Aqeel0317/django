a1b39c633c8ab34ee27ea73806244751720a9d44
Aqeel0317 <aqeel03319384502@gmail.com>
2025-02-23 07:45:03 +0500

    Update urls.py

--- shop/urls.py
+++ shop/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('search/', views.product_search, name='product_search'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+    path('categories/', views.category_list, name='category_list'),
     path('<slug:category_slug>/', views.product_list,name='product_list_by_category'),
     path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
    
